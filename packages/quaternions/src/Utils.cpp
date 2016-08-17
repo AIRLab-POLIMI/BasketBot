0a3f79a15a216691a6f3e2f973546b484a044503
Davide Tateo <boris.ilpossente@hotmail.it>
2016-08-17 11:55:11 +0200

    Fixed quaternion implementation
    - now should be ok
    - using wrong convention (wxyz instead of xyzw)

--- packages/quaternions/src/Utils.cpp
+++ packages/quaternions/src/Utils.cpp
@@ -7,17 +7,17 @@ namespace quaternions
 
 double Utils::getRoll(float q[4])
 {
-	return atan2(2.0*(q[1]*q[2]+q[0]*q[3]),(0.5*(q[2]*q[2]+q[3]*q[3])));
+	return atan2(q[1]*q[2]+q[3]*q[0], 0.5 - (q[0]*q[0]+q[1]*q[1]));
 }
 	
 double Utils::getPitch(float q[4])
 {
-	return asin(2.0*(q[0]*q[2] - q[3]*q[1]));
+	return asin(q[3]*q[1]-q[0]*q[2]);
 }
 	
 double Utils::getYaw(float q[4])
 {
-	return atan2((q[2]*q[3]+q[0]*q[1]),(0.5-(q[1]*q[1]+q[2]*q[2])));
+	return atan2(q[0]*q[1]+q[3]*q[2], 0.5 - (q[1]*q[1]+q[2]*q[2]));
 }
 
 
